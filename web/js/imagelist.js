de88fa9e06437d8f5afcc01f3a03ad6615613d1e
betrixed <michael.rynn.500@gmail.com>
2020-05-30 23:40:21 +1000

    update imagelist.js

--- web/js/imagelist.js
+++ web/js/imagelist.js
@@ -1,5 +1,21 @@
 // scripting for file.htm, to order by Asset
 // For GalleryAdmController
+
+// scripting for file.htm, to order by Asset
+function image_click(e) {
+    // set current session imageid
+    var imageid =  $(e.currentTarget).data('imgid');
+    var url="/admin/gallery/setid/" + imageid
+    $.post(url, {id:imageid},
+    function(data, status) {
+        $("div.img-row").each(function( index, elem) { 
+            var rowimg = elem.getAttribute('data-imgid');
+            var color = (rowimg === data) ? "yellow" : "#eee";
+            $(elem).css("background-color", color);   
+        } );
+    }); 
+}
+
 function post_imagelist(responseText, statusText, xhr, $form) {
     var status = $('#image_status');
     status.html(responseText);
@@ -36,6 +52,7 @@ function prep_imagelist() {
          var id = '#chk' + event.target.id.substring(4);
          $(id).prop('checked',true); 
     });
+  $("div.img-row").on("click", image_click);
 }
 $(document).ready(function() { 
         prep_imagelist();
